74cb872f63db3cb5e466ac7d4f8c0c2a498c1423
pedrohenriquemop <madeirapedrohenrique1112@gmail.com>
2025-04-29 22:07:28 -0300

    Commit 8: Replacing conditional with polymorphism - part 1

--- model.py
+++ model.py
@@ -53,6 +53,27 @@ class Rental:
         return self.book.get_frequent_renter_points(self.days_rented)
 
 
+class Price:
+
+    def get_charge(self, days_rented: int) -> float:
+        pass
+
+    def get_frequent_renter_points(self, days_rented: int) -> int:
+        pass
+
+
+class RegulaPrice(Price):
+    pass
+
+
+class NewReleasePrice(Price):
+    pass
+
+
+class ChildrenPrice(Price):
+    pass
+
+
 class Client:
 
     def __init__(self, name: str):
